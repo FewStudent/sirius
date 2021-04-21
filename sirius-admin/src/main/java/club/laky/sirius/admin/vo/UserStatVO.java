88af0ba6bebcaf95ec7457b548c3ff5e479f644b
panrulang <panrulang@linshimuye.com>
2021-04-21 18:28:12 +0800

    sirius 天狼小商场后台管理系统 购物车

---
+++ sirius-admin/src/main/java/club/laky/sirius/admin/vo/UserStatVO.java
@@ -0,0 +1,20 @@
+package club.laky.sirius.admin.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author panrulang
+ * @Desrcription:
+ * @date 2021/4/21 16:43
+ */
+@Data
+public class UserStatVO implements Serializable {
+    private String create_time;
+    private Integer create_count;
+    private Integer admin_count;
+    private Integer client_count;
+    private Double admin_create_rate;
+    private Double client_create_rate;
+}

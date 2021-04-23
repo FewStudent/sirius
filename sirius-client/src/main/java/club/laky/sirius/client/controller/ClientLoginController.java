c15612a55f508371f982cd5f8a439755ef2293d9
panrulang <panrulang@linshimuye.com>
2021-04-23 10:33:48 +0800

    sirius 天狼小商场平台 注册验证码

--- sirius-client/src/main/java/club/laky/sirius/client/controller/ClientLoginController.java
+++ sirius-client/src/main/java/club/laky/sirius/client/controller/ClientLoginController.java
@@ -1,6 +1,7 @@
 package club.laky.sirius.client.controller;
 
 import club.laky.sirius.client.feign.FeignClientService;
+import club.laky.sirius.client.provider.MailCreator;
 import club.laky.sirius.client.utils.WebResult;
 import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.UUID;
+
 
 @Controller
 @RequestMapping("/client/common")
@@ -22,6 +25,9 @@ public class ClientLoginController {
 
     @Autowired
     private FeignClientService clientService;
+
+    @Autowired
+    private MailCreator mailCreator;
 
     /**
      * 系统会员登录
@@ -73,5 +79,27 @@ public class ClientLoginController {
 
     //TODO 通过邮件获取验证码
 
+    /**
+     * 生成验证码
+     *
+     * @author panrulang
+     */
+    @ResponseBody
+    @RequestMapping("/generateCode")
+    public Object generateCode(@RequestParam String email) {
+        try {
+            logger.info("-------------生成验证码-------------");
+            String code = UUID.randomUUID().toString().replaceAll("-", "").substring(0, 6);
+            JSONObject params = new JSONObject();
+            params.put("email", email);
+            params.put("title", "注册验证码");
+            params.put("content", "您好,你的天狼小商场系统的注册验证码为：" + code);
+            mailCreator.send(params.toJSONString(), null);
+            return WebResult.success(code);
+        } catch (Exception e) {
+            logger.info("生成验证码失败:{}", e.getMessage());
+            return WebResult.error("生成验证码失败");
+        }
+    }
 
 }
